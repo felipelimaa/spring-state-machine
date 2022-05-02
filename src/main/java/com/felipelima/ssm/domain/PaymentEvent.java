b0826e57cb9f5b8a09e5d5e5de60cefef97ecd3f
Felipe Lima <fearlimasi@gmail.com>
2022-05-02 17:45:23 -0300

    feat: State and Event created for Payment

---
+++ src/main/java/com/felipelima/ssm/domain/PaymentEvent.java
@@ -0,0 +1,10 @@
+package com.felipelima.ssm.domain;
+
+public enum PaymentEvent {
+    PRE_AUTHORIZE,
+    PRE_AUTH_APPROVED,
+    PRE_AUTH_DECLINED,
+    AUTHORIZE,
+    AUTH_APPROVED,
+    AUTH_DECLINED
+}
---
+++ src/main/java/com/felipelima/ssm/domain/PaymentState.java
@@ -0,0 +1,9 @@
+package com.felipelima.ssm.domain;
+
+public enum PaymentState {
+    NEW,
+    PRE_ATH,
+    PRE_AUTH_ERROR,
+    AUTH,
+    AUTH_ERROR
+}
