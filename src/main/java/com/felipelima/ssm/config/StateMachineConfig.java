2a9efbb815f881b474762a6674266c3b692220fb
Felipe Lima <fearlimasi@gmail.com>
2022-05-02 18:04:41 -0300

    feat: Created StateMachineConfig

---
+++ src/main/java/com/felipelima/ssm/config/StateMachineConfig.java
@@ -0,0 +1,40 @@
+package com.felipelima.ssm.config;
+
+import com.felipelima.ssm.domain.PaymentEvent;
+import com.felipelima.ssm.domain.PaymentState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+@Slf4j
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.PRE_AUTH_ERROR)
+                .end(PaymentState.AUTH_ERROR);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
+        transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+                .and()
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+                .and()
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
+    }
+}
